b29f27847ba03d2542ed6ffc165abdfd520b714c
devops-champ <dndarshan2@gmail.com>
2025-10-21 20:56:54 +0530

    postgres docker image readme

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -27,13 +27,13 @@ CREATE TABLE public.activities (
 
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'abrown@gmail.com', 'MOCK'),
+  ('Darshan Dev', 'darshandev' , 'ddev@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' , 'abayko@gmail.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'darshandev' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
